91e5092300744fa0e6a5a62534aebbbfb5e47562
NabilRakhaD <nabilrakha22@gmail.com>
2022-11-17 23:06:17 +0700

    aboutPage Done

---
+++ about/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="../about.css">
+</head>
+<body>
+    <div class="tittle-container">
+        <img class="img" src="../img/aboutBekgron.png"/>
+        <p class="tittle">Together We Fight COVID-19</p>
+    </div>
+
+    <div class="content">
+        <div class="content1-1">
+            <div>
+                <h2> Tentang COVID-19 dan Penyebarannya </h2>
+                <p> Penyakit virus corona merupakan penyakit infeksi yang disebabkan oleh virus baru.
+                Penyakit ini menyerang orang pernafasan layaknya flu dengan gejala seperti 
+            batuk, demam, dan lain sebagainya, bahkan sesak napas </p>
+            </div>
+        </div>
+        <div class="content1-2">
+            <div class="subcontent">
+                <div class="img-wrapper">
+                    <img class="img-content" src="../img/contaminated.png"/>
+                </div>
+                <h4>Objek yang Terkontaminasi</h4>
+                <p>Seseorang bisa terkena COVID-19 dengan menyentuh sebuah permukaan
+                    atau objek yang ada virus pada permukaan atau objek tersebut dan
+                    kemudian orang itu menyentuh mulut atau hidungnya.
+                </p>
+            </div>    
+                
+            <div class="subcontent">
+                <div class="img-wrapper">
+                    <img class="img-content" src="../img/airtransmission.png"/>
+                </div>
+                <h4>Transmisi Udara</h4>
+                <p>Virus COVID-19 bisa menyebar lewat udara karena adanya orang yang terjangkit
+                    COVID-19 yang batuk ataupun bersin dan kemudian di hirup oleh orang terdekatnya
+                </p>
+            </div>
+
+            <div class="subcontent">
+                <div class="img-wrapper">
+                    <img class="img-content" src="../img/transmissioncontact.png"/>
+                </div>
+                <h4>Transmisi Antar Manusia</h4>
+                <p>Sama halnya dengan transmisi udara, seseorang bisa terjangkit virus COVID-19
+                    karena adanya kontak langsung atau bersentuhan dengan orang yang sudah terjangkit
+                    COVID-19
+                </p>
+            </div>
+
+            <div class="subcontent">
+                <div class="img-wrapper">
+                    <img class="img-content" src="../img/animal.png"/>
+                </div>
+                <h4>Transmisi dari Hewan</h4>
+                <p>Virus COVID-19 juga bisa menyebar lewat hewan-hewan yang terinfeksi.
+                    Hal ini seperti yang diberitakan kejadian awal munculnya COVID-19 yang
+                    berasal dari kelelewar.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="content-2">
+        <div class="content2-1">
+            <div class="content2-title">
+                <h2>Cara Menjaga Diri dari COVID-19</h2>
+            </div>
+            <div class="content2-ket">
+                <p>Tetap mengetahui informasi terbaru tentang wabah COVID-19, tersedia di situs web WHO 
+                dan melalui otoritas kesehatan masyarakat nasional dan lokal Anda</p>
+            </div>
+
+            <div class="content2-2">
+                <div class="subcontent-2">
+                    <div class="img-wrapper">
+                        <img class="img-content" src="../img/cucitangan.png"/>
+                    </div>
+                    <h4>Cuci Tangan</h4>
+                    <p>Biasakan bersihkan tangan dengan handsanitizer atau mencuci tangan dengan sabun
+                        setelah menyentuh apapun
+                    </p>
+                </div>    
+                    
+                <div class="subcontent-2">
+                    <div class="img-wrapper">
+                        <img class="img-content" src="../img/socialdistancing.png"/>
+                    </div>
+                    <h4>Menjaga Jarak</h4>
+                    <p>Jaga jarak 1 hingga 3 meter dari orang yang mengindikasikan gejala COVID-19 seperti 
+                        batuk ataupun bersin
+                    </p>
+                </div>
+
+                <div class="subcontent-2">
+                    <div class="img-wrapper">
+                        <img class="img-content" src="../img/touchface.png"/>
+                    </div>
+                    <h4>Jangan Sentuh Muka</h4>
+                    <p>Tangan yang menyentuh muka bisa meningkatkan seseorang terkena virus COVID-19. Maka 
+                        dari itu, biasakan jangan menyentuh bagian muka jika belum mencuci tangan dengan 
+                        sabun atau handsanitizer
+                    </p>
+                </div>
+
+                <div class="subcontent-2">
+                    <div class="img-wrapper">
+                        <img class="img-content" src="../img/stayhome.png"/>
+                    </div>
+                    <h4>Menetap di Rumah</h4>
+                    <p>Jika kita merasa kurang sehat, sebaiknya tetap menetap dirumah dibandingkan beraktivitas
+                        keluar rumah karena badan yang kurang sehat sangat rentan terhadap virus COVID-19 sehingga
+                        sangat berbahaya.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
