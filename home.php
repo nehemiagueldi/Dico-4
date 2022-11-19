9237e5f86942ea5be5efea05317835112ffe1cb9
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2022-11-19 20:06:47 +0700

    Update icon and title

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 
 <head>
    <meta charset="utf-8">
-   <title>DIAGNOSA COVID - 19</title>
+   <title>DICO-4</title>
    <meta content="width=device-width, initial-scale=1.0" name="viewport">
    <meta content="Free HTML Templates" name="keywords">
    <meta content="Free HTML Templates" name="description">
