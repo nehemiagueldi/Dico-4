ad9844b66c62659027647e5e805a0a7588a28201
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2022-12-03 19:05:41 +0700

    Update css and AOS

--- aboutus.php
+++ aboutus.php
@@ -126,15 +126,15 @@
 
 
   <!-- About Start -->
-  <div class="container-fluid py-5" data-aos="fade-up" data-aos-duration="3000">
+  <div class="container-fluid py-5">
     <div class="container">
       <div class="row gx-5">
         <div class="col-lg-5 mb-5 mb-lg-0" style="min-height: 500px;">
-          <div class="position-relative h-100">
-            <img class="position-absolute w-100 h-100 rounded" src="img/about.jpg" style="object-fit: cover;">
+          <div class="position-relative h-100" data-aos="fade-right" data-aos-duration="3000">
+            <img class="position-absolute w-100 h-100 rounded img-about" src="img/about.jpg" style="object-fit: cover;">
           </div>
         </div>
-        <div class="col-lg-7">
+        <div class="col-lg-7" data-aos="fade-left" data-aos-duration="3000">
           <div class="mb-4">
             <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5">About Us</h5>
             <h1 class="display-4">About COVID-19 and The Spread</h1>
