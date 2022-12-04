5dff7b43d40f8a71a268877481f0d7ef725fa882
NabilRakhaD <nabilrakha22@gmail.com>
2022-12-04 18:29:01 +0700

    pilgejala garis3 responsive

--- pil_gejala.php
+++ pil_gejala.php
@@ -116,4 +116,8 @@
         duration: 1800
       });
   </script>
+
+  <!-- Javascript -->
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
